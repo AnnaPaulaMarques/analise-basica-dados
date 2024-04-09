b32dbe77a0d08c96cdafbe674b6ca03e54ef9837
Anna Paula <annamarquess.silva@gmail.com>
2024-04-08 22:27:48 -0300

    Seletores com acumuladores e filtros

---
+++ aula0204/AULA0204.sql
@@ -0,0 +1,8 @@
+/* Modificadores 
+create , update , delete, insert
+Consultas
+select, view , temp, table, variavel*/
+
+SELECT * FROM DEPARTAMENTO;
+SELECT * FROM EMPREGADO;
+SELECT * FROM dependente;
